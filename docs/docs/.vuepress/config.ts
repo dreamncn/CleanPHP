748f9d0ffb95b606c16719893c66cf354a442834
ankio <dream@dreamn.cn>
2022-05-12 20:04:46 +0800

    feat: add crypto

--- docs/docs/.vuepress/config.ts
+++ docs/docs/.vuepress/config.ts
@@ -205,38 +205,26 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       },
     ],
 
-      [
-        '@vssue/vuepress-plugin-vssue', {
-  // 设置 `platform` 而不是 `api`
-  platform: 'github',
-
-        owner: 'dreamncn',
-        repo: 'issues',
-        clientId: 'ccde880a64714cebc411',
-        clientSecret: 'bf456704a2ec3e532974780be59cfe000fa5e5ba',
-        labels: ["cleanphp"],
-}
-      ],
-/*    [
+   [
       'vuepress-plugin-comment', // 评论
       {
         choosen: 'gitalk',
         options: {
-          clientID: 'a6e1355287947096b88b',
-          clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-          repo: 'blog-gitalk-comment', // GitHub 仓库
-          owner: 'xugaoyi', // GitHub仓库所有者
-          admin: ['xugaoyi'], // 对仓库有写权限的人
+          clientID: 'ccde880a64714cebc411',
+          clientSecret: 'bf456704a2ec3e532974780be59cfe000fa5e5ba',
+          repo: 'issues', // GitHub 仓库
+          owner: 'dreamncn', // GitHub仓库所有者
+          admin: ['dreamncn'], // 对仓库有写权限的人
           // distractionFreeMode: true,
           pagerDirection: 'last', // 'first'正序 | 'last'倒序
           id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
           title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-          labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
+          labels: ['cleanphp', 'Comment'], // GitHub issue 的标签
           body:
             '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
         },
       },
-    ],*/
+    ],
     [
       '@vuepress/last-updated', // "上次更新"时间格式
       {
---
+++ src/lib/Crypto/Crypto.php
@@ -0,0 +1,99 @@
+<?php
+/*******************************************************************************
+ * Copyright (c) 2022. CleanPHP. All Rights Reserved.
+ ******************************************************************************/
+/**
+ * Class Crypto
+ * Created By ankio.
+ * Date : 2022/4/2
+ * Time : 11:00
+ * Description :
+ */
+
+namespace app\lib\Encryption;
+
+class Crypto{
+    static ?Crypto $instance=null;
+    public static function getInstance(): ?Crypto
+    {
+        if (self::$instance == null) {
+            self::$instance = new static();
+        }
+        return self::$instance;
+    }
+
+    private function setKey($strLen, $key){
+        $key = md5($key);
+        $keyLen = strlen($key);
+        $skewy = $key;
+        if($keyLen>$strLen){
+            $skewy = substr($key,0,$strLen);
+        }else{
+            $limit = $strLen - $keyLen;
+            $j=0;
+            for($i=0;$i<$limit;$i++){
+                if($j>=$keyLen)
+                    $j=0;
+                $skewy.=$key[$j];
+                $j++;
+            }
+        }
+        return $skewy;
+    }
+
+    /**
+     * @param $str
+     * @param $key
+     * @return string
+     */
+
+    public function encrypt($str,$key): string
+    {
+        try{
+            $base = bin2hex($str);
+            $key = $this->setKey(strlen($base), $key);
+            // var_dump($key);
+            $strArr = str_split($base);
+            $keys = str_split($key);
+            $retArray = $strArr;
+            $randTable = [];
+            $keyCount = count($keys)-1;
+            foreach ($strArr as $item=>$value){
+                $rand = $keys[rand(0,$keyCount)];
+                $randTable[] = $rand;
+                $retArray[$item]= $retArray[$item]^$rand;
+            }
+            $encodeStr = join('', $retArray);
+            $randTable = join('',$randTable);
+            $table = $randTable^$key;
+            return str_rot13($encodeStr.$table);
+        }catch (\Throwable $e){
+            return "";
+        }
+    }
+
+    /**
+     * @param $str
+     * @param $key
+     * @return string
+     */
+    public function decrypt($str,$key): string
+    {
+       try{
+           $strLen = intval(strlen($str)/2);
+           $key = $this->setKey($strLen,$key);
+           $str = str_rot13($str);
+           $encodeData = str_split(substr($str,0,$strLen));
+           $strArr = str_split(substr($str,$strLen)^$key);
+           $retArray = $encodeData;
+           foreach ($strArr as $item=>$value){
+               $retArray[$item]= $retArray[$item]^ $value;
+           }
+           return hex2bin(join('', $retArray))??"";
+       }catch (\Throwable $e){
+           return "";
+       }
+    }
+
+
+}
